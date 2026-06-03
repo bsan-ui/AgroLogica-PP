d1f9d53898256437ef525989040deb0a624aff31
bsan-ui <brandon.sanchez8382@gmail.com>
2026-06-03 16:10:49 -0600

    Refactor web.py for improved diagnostics functionality
    Refactor code structure and improve data handling for soil diagnostics.

--- web.py
+++ web.py
@@ -168,7 +168,11 @@ with tab1:
 
             st.divider()
 
-            with st.expander("### Calculos realizados"):
+            with st.expander("Calculos realizados"):
+                cols_num=['pH', 'Pendiente %', 'Materia orgánica %', 'Nitrogeno mg/kg']
+                st.markdown("### 1.Estadística")
+                df_std=(df_limpio[cols_num]-df_limpio[cols_num].mean())/df_limpio[cols_num].std()
+                st.write(df_std.head(3))
         
 # =====================================================================
 # Pestaña 2: Diagnóstico manual
